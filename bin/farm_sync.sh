839df244879ea73f7b502952cbe0651d656f8809
os1077 <os1077@localhost>
2024-01-30 16:21:12 +0300

    commit05

--- bin/farm_sync.sh
+++ bin/farm_sync.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 BASE_CONF_DIR=/opt/kamp
-cd /tmp && rm -f kammconfiguration
+
+cd /tmp && rm -rf kammconfiguration
+
 cd /tmp && git clone  https://github.com/alimumcu1077/kampconfiguration.git \
 && cd /tmp/kampconfiguration  && git checkout main
 
-mkdir -p ${BASE_CONF_DIR} || echo "Base Configuration dizini olusturalamadi"; exit 1
-rsync -aP  /tmp/kampconfiguration/ --exclude=".git" ${BASE_CONF_DIR} 
+mkdir -p ${BASE_CONF_DIR} || echo "Base Configuration dizini olusturalamadi"
+
+
+rsync -aP  /tmp/kampconfiguration/ --exclude=".git" ${BASE_CONF_DIR}
